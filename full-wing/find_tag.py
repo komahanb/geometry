ba5eab8fddaa65972d2bb0d66ea30834d99ddc14
komahan <komahan.cool@gmail.com>
2016-12-13 00:04:20 -0500

    added code to identify BC nodes and apply bc

--- full-wing/find_tag.py
+++ full-wing/find_tag.py
@@ -15,17 +15,17 @@ def readInputFile(filename):
         raise IOError
     return content
 
-def loadMesh():
+def getElementID():
     inputfile = 'wing.bdf'
     bdf       = readInputFile(inputfile)
     tag  = []
     for line in bdf:
         entry = line.split()
         if entry[0] == "CQUAD9":
-            print entry[0], entry[1], int(entry[2])
+            # print entry[0], entry[1], int(entry[2])
             tag.append(int(entry[2]))
-    return tag
-        
+    return remove_duplicates(tag)
+
 def remove_duplicates(x):
     a = []
     for i in x:
@@ -33,10 +33,68 @@ def remove_duplicates(x):
             a.append(i)
     return a
 
-######################################################################
-#                    Setup truss
-######################################################################
+def getNodesForElementID(id):
+    inputfile = 'wing.bdf'
+    bdf       = readInputFile(inputfile)
+    node_idx  = []
+    skip = False
+    for line in bdf:
+        entry = line.split()
+        if entry[0] == "CQUAD9" and int(entry[2]) == id:
+            # Append the BC node indices to the list
+            node_idx.append(int(entry[3]))
+            node_idx.append(int(entry[4]))
+            node_idx.append(int(entry[5]))
+            node_idx.append(int(entry[6]))
+            node_idx.append(int(entry[7]))
+            node_idx.append(int(entry[8]))
+            
+            # parse the continuations next time
+            skip = True
+        elif skip:
+            # Append the BC node indices to the list
+            node_idx.append(int(entry[1]))
+            node_idx.append(int(entry[2]))
+            node_idx.append(int(entry[3]))
+            
+            # Now turn off the continuations
+            skip = False
+            
+    return sorted(remove_duplicates(node_idx))
 
-tags = loadMesh()
-unid = remove_duplicates(tags)
-print unid, len(unid)
+def writeSPC(id):
+    bc_nodes = getNodesForElementID(id)
+    # open file
+    fp = open('wing.bdf.bc', 'w')
+    # If BC nodes are present write as SPC entries
+    num_bc = len(bc_nodes)
+    if num_bc != 0:
+        # Set up the plate so that it is fully clamped
+        for i in xrange(num_bc):
+            # Set the y = const edges
+            spc = '123'
+            fp.write('%-8s%8d%8d%8s%8.6f\n'%
+                     ('SPC', 1, bc_nodes[i], spc, 0.0))
+
+        fp.write('END BULK')
+        fp.close()
+    return
+
+# Get all the elementID tags defined in the mesh
+print "Element IDs in the mesh"
+eid = getElementID()
+print eid
+print len(eid)
+
+id = 80
+print "Nodes belonging to ID:", id
+nodes = getNodesForElementID(id)
+print nodes
+print "Total nodes", len(nodes)
+
+for entry in nodes:
+    if entry == 4481:
+        print "yes"
+
+
+writeSPC(80)
